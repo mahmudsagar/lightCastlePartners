6e2e531c347f76c885e853d620d3dbc9f5511e05
mahmudsagar <mahmudsagar9@gmail.com>
2021-04-23 13:17:28 +0600

    about us edited

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,49 +2,27 @@ const body = document.querySelector("body");
 const currentDate = new Date();
 const currentyear = currentDate.getFullYear();
 
-//count up
-var aboutElement = document.querySelector(".aboutLCPartners");
-var aboutElementHeight = aboutElement.clientHeight;
-
-const inView = () => {
-    var windowHeight = window.innerHeight;
-    var scrollY = window.scrollY || window.pageYOffset;
-    var scrollPosition = scrollY + windowHeight;
-    var elementPosition =
-        aboutElement.getBoundingClientRect().top + scrollY + aboutElementHeight;
-    if (scrollPosition > elementPosition) {
-        return true;
-    }
-
-    return false;
+// count up function
+const count = document.querySelectorAll(".stat-count");
+const countUp = (item, start, end, duration) => {
+    if (start == end) return;
+    const range = end - start;
+    let current = start;
+    const stepTime = Math.abs(Math.floor(duration / range));
+    const timer = setInterval(function () {
+        current++;
+        item.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
 };
+count.forEach((item) => {
+    const value = item.innerHTML;
+    countUp(item, 0, value, 10000);
+});
 
-function animateValue(item, start, end, duration) {
-    if (inView()) {
-        if (start == end) return;
-        const range = end - start;
-        let current = start;
-        const stepTime = Math.abs(Math.floor(duration / range));
-        const timer = setInterval(function () {
-            current++;
-            item.innerHTML = current;
-            if (current == end) {
-                clearInterval(timer);
-            }
-        }, stepTime);
-    }
-}
-window.addEventListener('scroll', ()=>{
-    
-    const count = document.querySelectorAll(".stat-count");
-    count.forEach((item) => {
-        const value = item.innerHTML;
-        animateValue(item, 0, value, 5000);
-    });
-    
-})
-//featured video
-
+// video section
 const video = document.querySelectorAll(".video");
 const videoBtn = document.querySelectorAll(".image-overlay");
 const videoCloseBtn = document.querySelectorAll(".video-close");
