200e6cfc577b4c5d444660ebca50ed7a55e9db4c
mahmudsagar <mahmudsagar9@gmail.com>
2021-04-24 15:36:08 +0600

    testimonial added

--- javascripts/clientSlider.js
+++ javascripts/clientSlider.js
@@ -1,8 +1,8 @@
 // clients slider starts
 const clientContainer = document.querySelector(".clients__container");
 const clientSlider = document.querySelector(".clients__slider");
-const clientNexBtn = document.querySelector("#next-btn")
-const clientPrevBtn = document.querySelector("#prev-btn")
+const clientNexBtn = document.querySelector("#client-next-btn")
+const clientPrevBtn = document.querySelector("#client-prev-btn")
 const clientSlides = "clients__slide"
 
 slide(clientContainer, clientSlider, clientNexBtn, clientPrevBtn,clientSlides)
---
+++ javascripts/testimonial.js
@@ -0,0 +1,7 @@
+const testimonialContainer = document.querySelector(".testimonial__container");
+const testimoninalSlider = document.querySelector(".testimonial__slider");
+const testimoninalNexBtn = document.querySelector("#testimonial-next-btn")
+const testimoninalPrevBtn = document.querySelector("#testimonial-prev-btn")
+const testimoninalSlides = "testimonial__slide"
+
+slide(testimonialContainer, testimoninalSlider, testimoninalNexBtn, testimoninalPrevBtn,testimoninalSlides)
