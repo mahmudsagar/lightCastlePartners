e935e0db65e72f3a6a87ed35a887154fcd6f61f1
mahmudsagar <mahmudsagar9@gmail.com>
2021-04-24 15:49:17 +0600

    smooth scrolling added

--- javascripts/nav.js
+++ javascripts/nav.js
@@ -57,3 +57,36 @@ window.addEventListener("load", () => {
         contact.style.display = "none";
     }
 });
+// smooth scrolling
+const heroLinksContainer = document.querySelector(".sidenav");
+
+// select links
+const scrollLinks = document.querySelectorAll(".hero__page-map-btn");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // prevent default
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    // close
+    linksContainer.style.height = 0;
+  });
+});
